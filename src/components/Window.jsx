86b41361e0e758577d5d0d711833019f9ff278fc
kad <footnofetish123@gmail.com>
2024-07-05 22:48:07 -0400

    chore: Update DesktopApps and Window components to enable MusicPlayer functionality and show/hide logic

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -240,89 +240,89 @@ const Window = ({ show, onClose, defaultApp = 'C:\\' }) => {
           </div>
           <div className="flex-grow p-2">
             <div className="bg-white border border-gray-300 p-2 mb-2">
-              <div className="flex items-center mb-2">
+              <div className="flex                 items-center mb-2">
                 <div className="flex-grow text-left text-sm">
                   Address: 
                   <input
-                                      type="text"
-                                      value={address}
-                                      onChange={(e) => setAddress(e.target.value)}
-                                      className="ml-2 p-1 border border-gray-300 rounded"
-                                    />
-                                  </div>
-                                  <button className="bg-gray-300 p-1 rounded-full" onClick={handleGoClick}>Go</button>
-                                </div>
-                                <div className="flex space-x-4">
-                                  <div className="text-sm cursor-pointer">Back</div>
-                                  <div className="text-sm cursor-pointer">Search</div>
-                                  <div className="text-sm cursor-pointer">Folders</div>
-                                </div>
-                              </div>
-                              <div className="bg-white border border-gray-300 p-2">
-                                {showContacts ? (
-                                  <ul>
-                                    <li className="mb-2">Email: myleskmiller@gmail.com</li>
-                                    <p className="text-sm m-0"><a href="https://www.linkedin.com/in/myles-miller-669516263/">LinkedIn</a></p>
-                                    <li className="mb-2">Phone number: 678-559-2304</li>
-                                  </ul>
-                                ) : showProjectInfo ? (
-                                  <div>
-                                    <h2 className="font-bold text-lg mb-2">Projects</h2>
-                                    <menu> 
-                                      <a href="https://ozemoya.github.io/tictactoe/" className="text-sm text-blue-500 hover:underline mb-2">Tic-Tac-Toe</a>
-                                      <a href="https://ozemoya.github.io/investment-project/" className="text-sm text-blue-500 hover:underline mb-2">Investment Project</a>
-                                    </menu>
-                                  </div>
-                                ) : showServices ? (
-                                  <div>
-                                    <h2 className="font-bold text-lg mb-2">Services</h2>
-                                    <ul>
-                                      <li className="mb-2">Service 1: Soon :)</li>
-                                      <li className="mb-2">Service 2: Soon :)</li>
-                                      <li className="mb-2">Service 3: Soon :)</li>
-                                    </ul>
-                                  </div>
-                                ) : showMusic ? (
-                                  <div>
-                                    <h2 className="font-bold text-lg mb-2">Music</h2>
-                                    <ul>
-                                      <li className="mb-2">Song 1: Example Song 1</li>
-                                      <li className="mb-2">Song 2: Example Song 2</li>
-                                      <li className="mb-2">Song 3: Example Song 3</li>
-                                    </ul>
-                                  </div>
-                                ) : showDesktop ? (
-                                  <div>
-                                    <h2 className="font-bold text-lg mb-2">Desktop</h2>
-                                    <ul>
-                                      <li className="mb-2 cursor-pointer text-blue-500" onClick={handleProjectClick}>Projects</li>
-                                      <li className="mb-2 cursor-pointer text-blue-500" onClick={handleServicesClick}>Services</li>
-                                      <li className="mb-2 cursor-pointer text-blue-500" onClick={handleContactClick}>Contacts</li>
-                                    </ul>
-                                  </div>
-                                ) : (
-                                  <div>
-                                    <h2 className="font-bold text-lg mb-2">Quick Access</h2>
-                                    <ul>
-                                      {folderContent.map((item, index) => (
-                                        <li key={index} className="mb-2 cursor-pointer text-blue-500" onClick={item.onClick || null}>
-                                          <img src={item.icon} alt="" className="inline-block mr-2" />
-                                          {item.name}
-                                        </li>
-                                      ))}
-                                    </ul>
-                                    <h3 className="font-bold text-lg mt-4 mb-2">Recent</h3>
-                                    <ul>
-                                      <li className="mb-2">new folder icons</li>
-                                    </ul>
-                                  </div>
-                                )}
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  };
-                  
-                  export default Window;
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="ml-2 p-1 border border-gray-300 rounded"
+                  />
+                </div>
+                <button className="bg-gray-300 p-1 rounded-full" onClick={handleGoClick}>Go</button>
+              </div>
+              <div className="flex space-x-4">
+                <div className="text-sm cursor-pointer">Back</div>
+                <div className="text-sm cursor-pointer">Forward</div>
+                
+              </div>
+            </div>
+            <div className="bg-white border border-gray-300 p-2">
+              {showContacts ? (
+                <ul>
+                  <li className="mb-2">Email: myleskmiller@gmail.com</li>
+                  <p className="text-sm m-0"><a href="https://www.linkedin.com/in/myles-miller-669516263/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                  <li className="mb-2">Phone number: 678-559-2304</li>
+                </ul>
+              ) : showProjectInfo ? (
+                <div>
+                  <h2 className="font-bold text-lg mb-2">Projects</h2>
+                  <menu> 
+                    <a href="https://ozemoya.github.io/tictactoe/" target="_blank" rel="noopener noreferrer" className="text-sm text-blue-500 hover:underline mb-2">Tic-Tac-Toe</a>
+                    <a href="https://ozemoya.github.io/investment-project/" target="_blank" rel="noopener noreferrer" className="p-3 text-sm text-blue-500 hover:underline mb-2">Investment Project</a>
+                  </menu>
+                </div>
+              ) : showServices ? (
+                <div>
+                  <h2 className="font-bold text-lg mb-2">Services</h2>
+                  <ul>
+                    <li className="mb-2">Service 1: Soon :)</li>
+                    <li className="mb-2">Service 2: Soon :)</li>
+                    <li className="mb-2">Service 3: Soon :)</li>
+                  </ul>
+                </div>
+              ) : showMusic ? (
+                <div>
+                  <h2 className="font-bold text-lg mb-2">Music</h2>
+                  <ul>
+                    <li className="mb-2">Song 1: Example Song 1</li>
+                    <li className="mb-2">Song 2: Example Song 2</li>
+                    <li className="mb-2">Song 3: Example Song 3</li>
+                  </ul>
+                </div>
+              ) : showDesktop ? (
+                <div>
+                  <h2 className="font-bold text-lg mb-2">Desktop</h2>
+                  <ul>
+                    <li className="mb-2 cursor-pointer text-blue-500" onClick={handleProjectClick}>Projects</li>
+                    <li className="mb-2 cursor-pointer text-blue-500" onClick={handleServicesClick}>Services</li>
+                    <li className="mb-2 cursor-pointer text-blue-500" onClick={handleContactClick}>Contacts</li>
+                  </ul>
+                </div>
+              ) : (
+                <div>
+                  <h2 className="font-bold text-lg mb-2">Quick Access</h2>
+                  <ul>
+                    {folderContent.map((item, index) => (
+                      <li key={index} className="mb-2 cursor-pointer text-blue-500" onClick={item.onClick || null}>
+                        <img src={item.icon} alt="Folder Icon" className="inline-block mr-2" />
+                        {item.name}
+                      </li>
+                    ))}
+                  </ul>
+                  <h3 className="font-bold text-lg mt-4 mb-2">Recent</h3>
+                  <ul>
+                    <li className="mb-2">new folder icons</li>
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Window;
